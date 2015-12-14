e9af6c8535de5e4d483ae0171774f8e03056f2a9
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2015-12-14 19:36:49 +0100

    Fix invalid User to String conversion

--- src/main/java/pl/morecraft/dev/settler/service/converters/UserStringConverter.java
+++ src/main/java/pl/morecraft/dev/settler/service/converters/UserStringConverter.java
@@ -7,6 +7,6 @@ import pl.morecraft.dev.settler.domain.User;
 public class UserStringConverter extends AbstractConverter<User, String> {
     @Override
     protected String convert(User user) {
-        return user.getFirstName() + " " + user.getLastName() + "(" + user.getLogin() + ")";
+        return user.getFirstName() + " " + user.getLastName() + " (" + user.getLogin() + ")";
     }
 }
