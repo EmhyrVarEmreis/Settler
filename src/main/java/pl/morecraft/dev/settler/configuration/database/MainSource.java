78cb114526f6aea901839d2b32377bf7cd925bed
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2016-04-03 16:23:09 +0200

    Fix - Loading Spring Datasource configuration

--- src/main/java/pl/morecraft/dev/settler/configuration/database/MainSource.java
+++ src/main/java/pl/morecraft/dev/settler/configuration/database/MainSource.java
@@ -6,6 +6,7 @@ import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,16 +17,16 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
 @Configuration
-@ConfigurationProperties(prefix = "spring.datasource")
 @EnableTransactionManagement
 @MapperScan(basePackages = "pl.morecraft.dev.settler.dao.mapper", sqlSessionTemplateRef = "sqlSessionTemplate-settler")
 @EnableJpaRepositories("pl.morecraft.dev.settler.dao.repository")
-public class DatabaseConfiguration extends AbstractSource {
+public class MainSource {
 
     @Bean(name = "dataSource-settler")
+    @ConfigurationProperties(prefix = "spring.datasource")
     @Primary
     public DataSource getDataSource() {
-        return super.getDataSource();
+        return DataSourceBuilder.create().build();
     }
 
     @Bean(name = "sqlSession-settler")
