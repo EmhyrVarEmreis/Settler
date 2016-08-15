ff7fa32573d73838ae2888c72622bb3d13c12909
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2016-08-15 02:20:37 +0200

    Build improvements

--- src/main/java/pl/morecraft/dev/settler/Application.java
+++ src/main/java/pl/morecraft/dev/settler/Application.java
@@ -4,17 +4,26 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EnableAutoConfiguration(exclude = {
         DataSourceAutoConfiguration.class,
 })
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    private static Class<Application> applicationClass = Application.class;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
     }
 
 }
