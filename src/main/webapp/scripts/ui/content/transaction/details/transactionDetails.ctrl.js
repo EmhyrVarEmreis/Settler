c9637dd6a9e8a44fa3164576f7190f23ce24b613
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2016-12-06 23:24:29 +0100

    TransactionDetailsView - Improve form validation

--- src/main/webapp/scripts/ui/content/transaction/details/transactionDetails.ctrl.js
+++ src/main/webapp/scripts/ui/content/transaction/details/transactionDetails.ctrl.js
@@ -45,7 +45,10 @@
         };
 
         $scope.isFormValid = function () {
-            return !$scope.transactionDetailsForm.$invalid;
+            return !$scope.transactionDetailsForm.$invalid
+                && (!$scope.data.owners || $scope.data.owners.length < 1)
+                && (!$scope.data.categories || $scope.data.categories.length < 1)
+                && (!$scope.data.contractors || $scope.data.contractors.length < 1);
         };
 
         $scope.getPercentString = function (t, r) {
