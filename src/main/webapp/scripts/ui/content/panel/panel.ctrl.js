188a432ce95f874d906c1604d3f4eb2e7710e8bd
Krzysztof Kwiatkowski <krzysio94.vip@gmail.com>
2016-12-12 19:00:25 +0100

    Fix #11 - Double entries in total charges in panel - Cosmetic fix

--- src/main/webapp/scripts/ui/content/panel/panel.ctrl.js
+++ src/main/webapp/scripts/ui/content/panel/panel.ctrl.js
@@ -5,38 +5,38 @@
                                                                            totalChargeUserToFactory,
                                                                            totalChargeUserFromFactory) {
 
-$scope.charges = {};
+        $scope.charges = {};
 
-totalChargeUserToFactory.query({
-    id: null
-}).$promise.then(function (data) {
-    $scope.charges.to = data;
+        totalChargeUserToFactory.query({
+            id: null
+        }).$promise.then(function (data) {
+            $scope.charges.to = data;
 
-    totalChargeUserFromFactory.query({
-        id: null
-    }).$promise.then(function (data) {
-        $scope.charges.from = data;
+            totalChargeUserFromFactory.query({
+                id: null
+            }).$promise.then(function (data) {
+                $scope.charges.from = data;
 
-        for (var i = 0; i < $scope.charges.to.length; i++) {
-            for (var j = 0; j < $scope.charges.from.length; j++) {
-                if ($scope.charges.to[i].userFromId === $scope.charges.from[j].userToId) {
-                    if ($scope.charges.to[i].charge < $scope.charges.from[j].charge) {
-                        $scope.charges.from[j].charge = (parseFloat($scope.charges.from[j].charge)
-                        - parseFloat($scope.charges.to[i].charge)).toFixed(2);
-                        $scope.charges.to.splice(i, 1);
-                        i--;
-                    } else {
-                        $scope.charges.to[i].charge = (parseFloat($scope.charges.to[i].charge)
-                        - parseFloat($scope.charges.from[j].charge)).toFixed(2);
-                        $scope.charges.from.splice(j, 1);
-                        j--;
+                for (var i = 0; i < $scope.charges.to.length; i++) {
+                    for (var j = 0; j < $scope.charges.from.length; j++) {
+                        if ($scope.charges.to[i].userFromId === $scope.charges.from[j].userToId) {
+                            if ($scope.charges.to[i].charge < $scope.charges.from[j].charge) {
+                                $scope.charges.from[j].charge = (parseFloat($scope.charges.from[j].charge)
+                                - parseFloat($scope.charges.to[i].charge)).toFixed(2);
+                                $scope.charges.to.splice(i, 1);
+                                i--;
+                            } else {
+                                $scope.charges.to[i].charge = (parseFloat($scope.charges.to[i].charge)
+                                - parseFloat($scope.charges.from[j].charge)).toFixed(2);
+                                $scope.charges.from.splice(j, 1);
+                                j--;
+                            }
+                        }
                     }
                 }
-            }
-        }
 
-    });
-});
+            });
+        });
 
 
     });
