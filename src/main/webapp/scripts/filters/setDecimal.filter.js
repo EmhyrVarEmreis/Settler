85661f687191040b92cbe48c13a72b9ab158af7e
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-04-02 18:32:59 +0200

    Fix - Missing decimal places in Panel

--- src/main/webapp/scripts/filters/setDecimal.filter.js
+++ src/main/webapp/scripts/filters/setDecimal.filter.js
@@ -3,9 +3,18 @@
 
     angular.module('settlerApplication').filter('setDecimal', function() {
         return function(input, places) {
-            if (isNaN(input)) return input;
-            for (var i = input.toString().split(".")[1].length; i < places; i++) {
-                input += "0";
+            if (isNaN(input)) {
+                return input;
+            }
+            var n;
+            if (input.toString().indexOf('.') === -1 && input.toString().indexOf(',') === -1) {
+                input += '.';
+                n = 0;
+            } else {
+                n = input.toString().split(/[.,]/)[1].length;
+            }
+            for (var i = n; i < places; i++) {
+                input += '0';
             }
             return input;
         };
