6753ff79553d19634d6281c0035256f2ea84a601
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-06-10 23:04:37 +0200

    Panel - Added sum of charges

--- src/main/webapp/scripts/ui/content/panel/panel.ctrl.js
+++ src/main/webapp/scripts/ui/content/panel/panel.ctrl.js
@@ -1,39 +1,52 @@
-(function () {
+(function() {
     'use strict';
 
-    angular.module('settlerApplication').controller('PanelCtrl', function ($scope,
-                                                                           totalChargeUserToFactory,
-                                                                           totalChargeUserFromFactory,
-                                                                           categoryWithValueFactory,
-                                                                           userWithValueFactory) {
+    angular.module('settlerApplication').controller('PanelCtrl', function($scope,
+                                                                          totalChargeUserToFactory,
+                                                                          totalChargeUserFromFactory,
+                                                                          categoryWithValueFactory,
+                                                                          userWithValueFactory) {
 
-        $scope.charges = {};
+        $scope.charges = {
+            to:   {},
+            from: {}
+        };
 
         $scope.categoriesWithValue = categoryWithValueFactory.query();
         $scope.usersWithValue = userWithValueFactory.query();
 
         totalChargeUserToFactory.query({
             id: null
-        }).$promise.then(function (data) {
-            $scope.charges.to = data;
+        }).$promise.then(function(data) {
+            $scope.charges.to.list = data;
 
             totalChargeUserFromFactory.query({
                 id: null
-            }).$promise.then(function (data) {
-                $scope.charges.from = data;
+            }).$promise.then(function(data) {
+                $scope.charges.from.list = data;
+                var i, j;
 
-                for (var i = 0; i < $scope.charges.to.length; i++) {
-                    for (var j = 0; j < $scope.charges.from.length; j++) {
-                        if ($scope.charges.to[i].userFromId === $scope.charges.from[j].userToId) {
-                            if ($scope.charges.to[i].charge < $scope.charges.from[j].charge) {
-                                $scope.charges.from[j].charge = (parseFloat($scope.charges.from[j].charge)
-                                - parseFloat($scope.charges.to[i].charge)).toFixed(2);
-                                $scope.charges.to.splice(i, 1);
+                $scope.charges.to.sum = 0;
+                for (i = 0; i < $scope.charges.to.list.length; i++) {
+                    $scope.charges.to.sum += $scope.charges.to.list[i].charge;
+                }
+                $scope.charges.from.sum = 0;
+                for (i = 0; i < $scope.charges.from.list.length; i++) {
+                    $scope.charges.from.sum += $scope.charges.from.list[i].charge;
+                }
+
+                for (i = 0; i < $scope.charges.to.list.length; i++) {
+                    for (j = 0; j < $scope.charges.from.list.length; j++) {
+                        if ($scope.charges.to.list[i].userFromId === $scope.charges.from.list[j].userToId) {
+                            if ($scope.charges.to.list[i].charge < $scope.charges.from.list[j].charge) {
+                                $scope.charges.from.list[j].charge = (parseFloat($scope.charges.from.list[j].charge)
+                                - parseFloat($scope.charges.to.list[i].charge)).toFixed(2);
+                                $scope.charges.to.list.splice(i, 1);
                                 i--;
                             } else {
-                                $scope.charges.to[i].charge = (parseFloat($scope.charges.to[i].charge)
-                                - parseFloat($scope.charges.from[j].charge)).toFixed(2);
-                                $scope.charges.from.splice(j, 1);
+                                $scope.charges.to.list[i].charge = (parseFloat($scope.charges.to.list[i].charge)
+                                - parseFloat($scope.charges.from.list[j].charge)).toFixed(2);
+                                $scope.charges.from.list.splice(j, 1);
                                 j--;
                             }
                         }
