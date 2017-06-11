dc7a5dd8f3756f50fce83fcc36b56d42995d8ea4
Mateusz Stefaniak <mateusz.stefaniak@morecraft.pl>
2017-06-11 02:29:40 +0200

    Added authorization filters

--- src/main/java/pl/morecraft/dev/settler/security/authorisation/PermissionManager.java
+++ src/main/java/pl/morecraft/dev/settler/security/authorisation/PermissionManager.java
@@ -7,15 +7,21 @@ import pl.morecraft.dev.settler.domain.dictionaries.OperationType;
 
 public interface PermissionManager {
 
-    //void authorize(PrivilegeObject target, OperationType operationType);
+    void authorize(Long targetId, OperationType operationType);
+
+    void authorize(PrivilegeObject target, OperationType operationType);
+
+    void authorize(Long sourceId, Long targetId, OperationType operationType);
 
     void authorize(PrivilegeObject source, PrivilegeObject target, OperationType operationType);
 
+    boolean isAuthorized(Long targetId, OperationType operationType);
 
-    //boolean isAuthorized(PrivilegeObject target, OperationType operationType);
+    boolean isAuthorized(PrivilegeObject target, OperationType operationType);
+
+    boolean isAuthorized(Long sourceId, Long targetId, OperationType operationType);
 
     boolean isAuthorized(PrivilegeObject source, PrivilegeObject target, OperationType operationType);
-
 
     BooleanExpression objectFilter(PrivilegeObject source, QPrivilegeObject target, OperationType operationType);
 
